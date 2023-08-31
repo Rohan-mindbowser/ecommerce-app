6100572c6ce10c3118b290cf3c45f4b9d0b7126b
Rohan Kadam <rohan.kadam@mindbowser.com>
2023-08-31 19:15:47 +0530

    view single product route setup

--- app/components/ProductSlider.tsx
+++ app/components/ProductSlider.tsx
@@ -4,6 +4,7 @@ import "swiper/css";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import Image from "next/image";
+import Link from "next/link";
 
 const responsive = {
   desktop: {
@@ -35,81 +36,101 @@ const ProductSlider = () => {
       infinite={true}
       className="mb-12"
     >
-      <div className="w-full px-2">
-        <Image
-          src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.1/h_515,c_limit/ac871484-8738-44cf-abbd-4d347ad536e9/revolution-6-flyease-easy-on-off-road-running-shoes-XvR99K.png"
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: "100%", height: "auto" }} // optional
-          alt="home image"
-        />
-        <div className="flex justify-between mt-2">
-          <h1>Nike Revolution 6 FlyEase</h1>
-          <span>₹ 3 695</span>
+      <Link href="/pages/product/890asdasd">
+        <div className="w-full px-2">
+          <Image
+            src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.1/h_515,c_limit/ac871484-8738-44cf-abbd-4d347ad536e9/revolution-6-flyease-easy-on-off-road-running-shoes-XvR99K.png"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: "100%", height: "auto" }} // optional
+            alt="home image"
+          />
+          <div className="flex justify-between mt-2">
+            <h1>Nike Revolution 6 FlyEase</h1>
+            <span>₹ 3 695</span>
+          </div>
+          <h1 className="text-gray-500">
+            Men's Easy On/Off Road Running Shoes
+          </h1>
         </div>
-        <h1 className="text-gray-500">Men's Easy On/Off Road Running Shoes</h1>
-      </div>
-      <div className="w-full px-2">
-        <Image
-          src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.1/h_515,c_limit/a5acfc22-bbb0-4630-ba26-a6593d45ebc5/jordan-1-mid-younger-shoes-FKKgKz.png"
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: "100%", height: "auto" }} // optional
-          alt="home image"
-        />
-        <div className="flex justify-between mt-2">
-          <h1>Nike Revolution 6 FlyEase</h1>
-          <span>₹ 3 695</span>
+      </Link>
+      <Link href="/pages/product/89sassxxxx0asdasd">
+        <div className="w-full px-2">
+          <Image
+            src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.1/h_515,c_limit/a5acfc22-bbb0-4630-ba26-a6593d45ebc5/jordan-1-mid-younger-shoes-FKKgKz.png"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: "100%", height: "auto" }} // optional
+            alt="home image"
+          />
+          <div className="flex justify-between mt-2">
+            <h1>Nike Revolution 6 FlyEase</h1>
+            <span>₹ 3 695</span>
+          </div>
+          <h1 className="text-gray-500">
+            Men's Easy On/Off Road Running Shoes
+          </h1>
         </div>
-        <h1 className="text-gray-500">Men's Easy On/Off Road Running Shoes</h1>
-      </div>
-      <div className="w-full px-2">
-        <Image
-          src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.1/h_515,c_limit/889d7ede-9a1a-4127-9aae-450a84a355ea/legend-essential-3-next-nature-workout-shoes-sJBRlm.png"
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: "100%", height: "auto" }} // optional
-          alt="home image"
-        />
-        <div className="flex justify-between mt-2">
-          <h1>Nike Revolution 6 FlyEase</h1>
-          <span>₹ 3 695</span>
+      </Link>
+      <Link href="/pages/product/8900897678asdasd">
+        <div className="w-full px-2">
+          <Image
+            src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.1/h_515,c_limit/889d7ede-9a1a-4127-9aae-450a84a355ea/legend-essential-3-next-nature-workout-shoes-sJBRlm.png"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: "100%", height: "auto" }} // optional
+            alt="home image"
+          />
+          <div className="flex justify-between mt-2">
+            <h1>Nike Revolution 6 FlyEase</h1>
+            <span>₹ 3 695</span>
+          </div>
+          <h1 className="text-gray-500">
+            Men's Easy On/Off Road Running Shoes
+          </h1>
         </div>
-        <h1 className="text-gray-500">Men's Easy On/Off Road Running Shoes</h1>
-      </div>
-      <div className="w-full px-2">
-        <Image
-          src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.1/h_515,c_limit/81183b86-63a5-47b0-bc6b-cf54b456a858/nikecourt-legacy-older-shoes-dBCJpZ.png"
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: "100%", height: "auto" }} // optional
-          alt="home image"
-        />
-        <div className="flex justify-between mt-2">
-          <h1>Nike Revolution 6 FlyEase</h1>
-          <span>₹ 3 695</span>
+      </Link>
+      <Link href="/pages/product/890asdajhkhuyt564sd">
+        <div className="w-full px-2">
+          <Image
+            src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.1/h_515,c_limit/81183b86-63a5-47b0-bc6b-cf54b456a858/nikecourt-legacy-older-shoes-dBCJpZ.png"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: "100%", height: "auto" }} // optional
+            alt="home image"
+          />
+          <div className="flex justify-between mt-2">
+            <h1>Nike Revolution 6 FlyEase</h1>
+            <span>₹ 3 695</span>
+          </div>
+          <h1 className="text-gray-500">
+            Men's Easy On/Off Road Running Shoes
+          </h1>
         </div>
-        <h1 className="text-gray-500">Men's Easy On/Off Road Running Shoes</h1>
-      </div>
-      <div className="w-full px-2">
-        <Image
-          src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.1/h_515,c_limit/f9aec2b4-e2b6-45bf-b0cd-99574d0266cd/revolution-6-road-running-shoes-vjrrwc.png"
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: "100%", height: "auto" }} // optional
-          alt="home image"
-        />
-        <div className="flex justify-between mt-2">
-          <h1>Nike Revolution 6 FlyEase</h1>
-          <span>₹ 3 695</span>
+      </Link>
+      <Link href="/pages/product/iii8979iouasd">
+        <div className="w-full px-2">
+          <Image
+            src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.1/h_515,c_limit/f9aec2b4-e2b6-45bf-b0cd-99574d0266cd/revolution-6-road-running-shoes-vjrrwc.png"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: "100%", height: "auto" }} // optional
+            alt="home image"
+          />
+          <div className="flex justify-between mt-2">
+            <h1>Nike Revolution 6 FlyEase</h1>
+            <span>₹ 3 695</span>
+          </div>
+          <h1 className="text-gray-500">
+            Men's Easy On/Off Road Running Shoes
+          </h1>
         </div>
-        <h1 className="text-gray-500">Men's Easy On/Off Road Running Shoes</h1>
-      </div>
+      </Link>
     </Carousel>
   );
 };
